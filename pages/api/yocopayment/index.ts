b7739f72042fda527b8700536b3f3448f7c9e328
shaun <modipanesh@gmail.com>
2022-11-11 11:33:31 +0200

    first-ish commit

---
+++ pages/api/yocopayment/index.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse,
+) {
+	if (req.method === 'POST') {
+		try {
+			const result = await axios.post(
+				'https://online.yoco.com/v1/charges/',
+				{
+					token: req.body,
+					amountInCents: 2499,
+					currency: 'ZAR',
+				},
+				{
+					headers: {
+						'X-Auth-Secret-Key': process.env.YOCO_SECRET_KEY,
+					},
+				},
+			);
+			console.log(result);
+			res.status(200).json({ status: 'OK' });
+		} catch (error) {
+			console.log(error);
+			res.status(500).json({ status: 'not ok' });
+		}
+	}
+}
